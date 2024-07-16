f4c25c001331e69d864be72fad766cd6b5ab49b5
Elizabeth <itsalaz@icloud.com>
2024-07-16 08:57:58 -0400

    lab worked on

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
-#!/usr/bin/env python3
+
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            raise ValueError("page_count must be an integer")
+        self._page_count = value
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        self._title = value
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+        
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="Adidas", size=9):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        self._size = value
+        
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
